75be1428b0bd21bd8d679e6f835b16b18f4ee781
Yutaroh <yutaroh.tech@gmail.com>
2023-04-06 08:36:40 -0400

    modified

--- clip.py
+++ clip.py
@@ -1,56 +1,60 @@
 class Point:
-    x: int
-    y: int
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
 
 class LineSegment:
-    a: Point 
-    b: Point 
-
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
 
 class Triangle:
-    a: Point 
-    b: Point
-    c: Point
+    def __init__(self,a,b,c):
+        self.a = a
+        self.b = b
+        self.c = c
 
-
-def next_point(origin, triangles) -> Point:
+def next_point(origin, linesegment) -> Point:
     """
     Given a polygon and a point returns the next point clockwise.
     """
-    for seg in triangles
-        if neigbors(origin, seg):
-            return seg.a or seg.b
+    if origin.a == linesegment.a or origin.b == linesegment.a:
+        return linesegment.b
+    else:
+        return linesegment.a
         
 def neighbors(origin, seg) -> bool:
     """
     Given two line segments returns true if two points are connected.
     """
-    if origin.a or origin.b is same as seg.a or seg.b
-    return true
-    else
-    return false
+    if set[origin.a, origin.b] & set[seg.a, seg.b]:
+        return True
+    else:
+        return False
+
+
 
 
 def left_most_segment(polygon) -> LineSegment:
     """
     Given a polygon returns the left most line segment clockwise.
     """
-    for linesegment in polygon:
-        #get smallest value of x 
-    return linesegment with the point with the smallest x value
+    #smallest_line = polygon[0].a
+    #for linesegment in polygon   
+    #return linesegment with the point with the smallest x value
 
+    return polygon[0]
 
 def make_triangle_from_line(origin, seg, triangles) -> Triangle:
     """
     Given a polygon returns the left most point.
     """
     if len(triangles) == 1:
-        t = triangles[0]
-        t.a = origin
-        t.c = seg
+        triangles[0] = Triangle(origin.a, origin.b, seg.a or seg.b)
+        
     else:
-        t = triangles.pop()
+        #t = triangles.pop()
         t_new = Triangle(t.a, seg, t.c)
         triangles.append(t)
         triangles.append(t_new)
@@ -61,11 +65,27 @@ def ear_clip_mono_polygon(polygon):
     Given a polygon returns the left most point.
     """
     # this is not important which point is picked
-    origin = left_most_segment(polygon)
-    triangles = [Triangle(a=None, b=next_point(origin, polygon), c=None)]
+    origin = polygon[0]
+    neighbore_seg = None
     for seg in polygon:
-        if not neighbors(seg, origin):
-            tri = make_triangle_from_line(origin, seg, triangles)
-            triangles.append(tri)
-        else:
-            continue
+        if neighbors(origin,seg):
+            neighbore_seg = seg
+            break
+
+    triangles = [Triangle(a=origin.a, b=next_point(origin,neighbore_seg), c=origin.b)]
+    #remove origin and a segment in the first triangle from polygon
+
+    for seg in polygon:
+        if neighbors(seg, neighbore_seg):
+            if (they can make triangle within the polygon):
+                triangles.append[Triangle(a=neighbore_seg.a, b=next_point(neighbore_seg,seg), c=neighbore_seg.b)]
+
+
+
+    
+    #for seg in polygon:
+    #    if not neighbors(seg, origin):
+    #        tri = make_triangle_from_line(origin, seg, triangles)
+    #        #triangles.append(tri)
+    #    else:
+    #        continue
