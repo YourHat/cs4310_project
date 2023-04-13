243a5101b4aa3e1bf006a302e8a7464509629ff0
Devin Ragotzy <devin.ragotzy@gmail.com>
2023-04-13 16:40:12 -0400

    Ear clipping works with generated polygons and matplotlib

--- clip.py
+++ clip.py
@@ -17,14 +17,14 @@ class Triangle: #a,b,c are Point class
         self.b = b
         self.c = c
 
-        
+
 def neighbors(origin, seg) -> bool:
     """
     Given two line segments returns true if two points are connected.
     """
 
     # if two lines have a same point, then return true
-    if set(((seg.a.x, seg.a.y),(seg.b.x, seg.b.y))) & set(((origin.a.x, origin.a.y),(origin.b.x, origin.b.y))): 
+    if set(((seg.a.x, seg.a.y),(seg.b.x, seg.b.y))) & set(((origin.a.x, origin.a.y),(origin.b.x, origin.b.y))):
         return True
     else:
         return False
@@ -35,7 +35,7 @@ def is_there_a_point(seg, seg_next, polygon) -> bool:
     two line with three points (origin_point - middle point, head_point, and tail_point)
     """
     # get all three points from the two given lines
-    origin_point = set(((seg.a.x, seg.a.y),(seg.b.x, seg.b.y))) & set(((seg_next.a.x, seg_next.a.y),(seg_next.b.x, seg_next.b.y))) 
+    origin_point = set(((seg.a.x, seg.a.y),(seg.b.x, seg.b.y))) & set(((seg_next.a.x, seg_next.a.y),(seg_next.b.x, seg_next.b.y)))
     origin_point = Point(list(origin_point)[0][0],list(origin_point)[0][1])
     if seg.a.x == origin_point.x and seg.a.y == origin_point.y:
         head_point = seg.b
@@ -50,7 +50,7 @@ def is_there_a_point(seg, seg_next, polygon) -> bool:
     calculate area of triangle with the two lines
     if the sum of triangles made with a point in polygon is the same, then return false
     """
-    area_of_triangle = area_x2(origin_point.x, origin_point.y,head_point.x, head_point.y, tail_point.x, tail_point.y) 
+    area_of_triangle = area_x2(origin_point.x, origin_point.y,head_point.x, head_point.y, tail_point.x, tail_point.y)
     for line in polygon:
         #skip if a point has the same coordinate as the three points for the triangle
         if (line.a.x, line.a.y) not in ((origin_point.x, origin_point.y),(head_point.x, head_point.y),(tail_point.x,tail_point.y)):
@@ -71,14 +71,14 @@ def area_x2(x1,y1,x2,y2,x3,y3) -> int:
     didnt want to cut in half, sicne you might get float value
     """
     return abs((x1 * (y2 -y3)) + (x2 * (y3 - y1)) + (x3 * (y1 - y2)))
-        
+
 
 def left_two_most_seg(polygon) -> tuple:
     """
     returns two of the left most line segments for initialization
     left_up is the line segment that goes up from the left most point
     left_down is the line segment that goes down from the left most point
-    it is to go though polygon clockwise 
+    it is to go though polygon clockwise
     """
     left_up = polygon[0]
     left_down = None
@@ -91,7 +91,7 @@ def left_two_most_seg(polygon) -> tuple:
         temp = left_up
         left_up = left_down
         left_down = temp
-    
+
     return left_up, left_down
 
 
@@ -99,7 +99,7 @@ def is_convex(seg, seg_next) -> bool:
     """
     returns true if two line segments are convex (less than 180 degrees)
     """
-    origin_point = set(((seg.a.x, seg.a.y),(seg.b.x, seg.b.y))) & set(((seg_next.a.x, seg_next.a.y),(seg_next.b.x, seg_next.b.y))) 
+    origin_point = set(((seg.a.x, seg.a.y),(seg.b.x, seg.b.y))) & set(((seg_next.a.x, seg_next.a.y),(seg_next.b.x, seg_next.b.y)))
     origin_point = Point(list(origin_point)[0][0],list(origin_point)[0][1])
 
     if seg.a.x == origin_point.x and seg.a.y == origin_point.y:
@@ -110,7 +110,7 @@ def is_convex(seg, seg_next) -> bool:
         tail_point = seg_next.b
     else:
         tail_point = seg_next.a
-    
+
     if (((head_point.x - origin_point.x) * (tail_point.y - origin_point.y)) -  ((head_point.y - origin_point.y) * (tail_point.x - origin_point.x))) > 0:
         return True
     else:
@@ -120,7 +120,7 @@ def make_triangle(seg, seg_next, triangles) -> LineSegment:
     """
     create a triangle with two given line segments, and returns line segment that is created to create the triangle
     """
-    origin_point = set(((seg.a.x, seg.a.y),(seg.b.x, seg.b.y))) & set(((seg_next.a.x, seg_next.a.y),(seg_next.b.x, seg_next.b.y))) 
+    origin_point = set(((seg.a.x, seg.a.y),(seg.b.x, seg.b.y))) & set(((seg_next.a.x, seg_next.a.y),(seg_next.b.x, seg_next.b.y)))
     origin_point = Point(list(origin_point)[0][0],list(origin_point)[0][1])
     if seg.a.x == origin_point.x and seg.a.y == origin_point.y:
         head_point = seg.b
@@ -130,32 +130,32 @@ def make_triangle(seg, seg_next, triangles) -> LineSegment:
         tail_point = seg_next.b
     else:
         tail_point = seg_next.a
-        
+
     triangles.append(Triangle(head_point, origin_point, tail_point))
     return LineSegment(head_point, tail_point)
 
-def ear_clip_mono_polygon(polygon) -> list:
+def ear_clip_mono_polygon(polygon) -> list[Triangle]:
     """
     Given a polygon returns a list of triangles made by triangulation - ear clipping
     """
     # declare triangles list as global variable, so that I can access to it from the make_triangle function
-    global triangles 
+    global triangles
     triangles = []
 
     seg_next, seg_current = left_two_most_seg(polygon)
-    
+
     found_tri = False #change it to True if there is a triangle you can create with a line segment
     seg_temp = None # used when there is no triangle you can create with a line segment
     while(len(polygon) > 3): #stop when there are three line segments left -> then create a triangle from the lines
         for seg in polygon:
             if neighbors(seg, seg_next):
                 # if the neighbore line is counterclock wise, return false
-                if not (set(((seg.a.x, seg.a.y),(seg.b.x, seg.b.y))) & set(((seg_current.a.x, seg_current.a.y),(seg_current.b.x, seg_current.b.y)))): 
+                if not (set(((seg.a.x, seg.a.y),(seg.b.x, seg.b.y))) & set(((seg_current.a.x, seg_current.a.y),(seg_current.b.x, seg_current.b.y)))):
                     seg_temp = seg #used when there is no triangle you can create with a line segment
                     if is_convex(seg_next, seg):
-                        if is_there_a_point(seg_next,seg,polygon):
+                        if is_there_a_point(seg_next, seg, polygon):
                             # get rid of line segments used to create triangle from polygon list
-                            if seg in polygon: 
+                            if seg in polygon:
                                 polygon.remove(seg)
                             if seg_next in polygon:
                                 polygon.remove(seg_next)
@@ -215,4 +215,4 @@ def ex2():
 #    ex2()
 
 #temp_main()
-                     
+
